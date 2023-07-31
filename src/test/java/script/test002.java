8a34a4d75b4b3d6503c4282b73a34045bb171c57
HP <HP@DESKTOP-8M9HAN5>
2023-07-31 23:34:06 +0530

    hello

---
+++ src/test/java/script/test002.java
@@ -0,0 +1,11 @@
+package script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class test002 {
+	@Test
+	public void tcs02(){
+		Reporter.log("hello shubh", true);
+	}
+}
